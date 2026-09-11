3a85cfe9225f661aa5c21e6f1b76fb082d9902cf
jhsolutioncaisse-pixel <jhsolutioncaisse@gmail.com>
2026-09-11 13:17:25 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- veri2.php
+++ veri2.php
@@ -4,7 +4,7 @@
 // CONNEXION BASE DE DONNEES - CLEVER CLOUD
 // =========================================================
 
-$host   = "localhost";
+$host   = "%";
 $dbname = "u728826376_BATEAUJH";
 $user   = "u728826376_nzobazolahenoc";
 $pass   = "Henoc@2001";
